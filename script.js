58a92b87802770d8fa46c60e9c88c365c615bed1
Serban-Petru <177335339+Serban-Petru@users.noreply.github.com>
2025-10-31 21:03:13 +0200

    modificari noi si noua structura a site-ului

--- script.js
+++
@@ -1,8 +0,0 @@
-// ===================== MENIU MOBIL =====================
-// Deschide / închide meniul când se apasă pe icon-ul "☰"
-const menuToggle = document.getElementById('menuToggle');
-const navLinks = document.getElementById('navLinks');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
